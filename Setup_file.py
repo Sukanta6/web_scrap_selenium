a9a11bcac9032bb7814c18e891274f26e2c0a38c
sukanta <sukanta6@gmail.com>
2024-01-23 22:19:26 +0530

    web request

--- Setup_file.py
+++ Setup_file.py
@@ -7,12 +7,34 @@
 # Install Library: python -m pip install matplotlib 
 # python -m pip install numpy
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import requests
 
 def test():
  x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
  plt.plot(x, np.sin(x))       # Plot the sine of each x point
  plt.show()                   # Display the plot
 
-test()
+def list():
+    ls=[1,2,3,4,5,6]
+    print(ls)
+    print(type(ls))
+    print(ls[:5])
+
+def req():
+  
+    url = "https://reqres.in/api/users/5"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    data = data["data"]
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df = df.T
+    print(response.text)
+
+if __name__== "__main__":
+    req()
+    # test()
